661df87d000e1e61ac272bb197399acd21d9e5ab
Daniela Venuta <daniela.venuta27@gmail.com>
2022-04-09 23:09:17 -0400

    Import weapons

---
+++ Hell's Kitchen/Assets/Scripts/Weapons/Sword.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    public float Damage = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        throw new NotImplementedException();
+    }
+}
