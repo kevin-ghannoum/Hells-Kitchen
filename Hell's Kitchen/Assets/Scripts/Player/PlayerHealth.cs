85cbbc509d92e6af833fe60c8a472f45889e2153
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2022-04-09 23:29:58 -0400

    Adds decorated restaurant scene (#43)
    * Add cooking assets
    
    * Create Restaurant Scene
    
    * Small improvements

--- Hell's Kitchen/Assets/Scripts/Player/PlayerHealth.cs
+++ Hell's Kitchen/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,32 +1,35 @@
-using Player;
+using Common.Interfaces;
 using UnityEngine;
 using UnityEngine.Events;
-using Common.Interfaces;
-public class PlayerHealth : MonoBehaviour, IKillable
+
+namespace Player
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] private float _hitPoints = 100;
-    private UnityEvent _killed;
-    public UnityEvent Killed => _killed ??= new UnityEvent();
-
-    public float HitPoints
+    public class PlayerHealth : MonoBehaviour, IKillable
     {
-        get => _hitPoints;
-        set => _hitPoints = value;
-    }
+        [SerializeField] private Animator animator;
+        [SerializeField] private float _hitPoints = 100;
+        private UnityEvent _killed;
+        public UnityEvent Killed => _killed ??= new UnityEvent();
+
+        public float HitPoints
+        {
+            get => _hitPoints;
+            set => _hitPoints = value;
+        }
     
-    public void TakeDamage(float damage)
-    {
-        animator.SetTrigger(PlayerAnimator.TakeHit);
-        HitPoints -= damage;
+        public void TakeDamage(float damage)
+        {
+            animator.SetTrigger(PlayerAnimator.TakeHit);
+            HitPoints -= damage;
 
-        // If the player's hp is at 0 or lower, they die
-        if (HitPoints <= 0)
-            Die();
-    }
+            // If the player's hp is at 0 or lower, they die
+            if (HitPoints <= 0)
+                Die();
+        }
 
-    private void Die()
-    {
-        Killed.Invoke();
+        private void Die()
+        {
+            Killed.Invoke();
+        }
     }
 }
