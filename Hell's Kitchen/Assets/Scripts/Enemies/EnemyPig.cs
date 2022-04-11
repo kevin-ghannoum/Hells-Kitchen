7b88b657bf950de2cfe141d5f67aed96173f14ab
kevin-ghannoum <kevin.ghannoum@gmail.com>
2022-04-11 16:06:35 -0400

    Added the pig and added to the dungeon. Fixed animation and added audio

---
+++ Hell's Kitchen/Assets/Scripts/Enemies/EnemyPig.cs
@@ -0,0 +1,76 @@
+using System;
+using Common.Enums;
+using Enemies.Enums;
+using Player;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyPig : Enemy
+    {
+        [Header("Parameters")]
+        [SerializeField]
+        private float attackRate = 0.5f;
+
+        [SerializeField]
+        private float aggroRadius = 20.0f;
+
+        private PlayerController _player;
+        private float _lastAttack;
+
+
+        [SerializeField]
+        private float chargeSpeed = 10;
+        [SerializeField]
+        private float chargeTime = 3;
+        private float currentChargeTime = 0;
+
+        private Vector3 chargeDirection;
+        private Rigidbody rb;
+
+
+        private void Start()
+        {
+            _player = GameObject.FindWithTag(Tags.Player).GetComponent<PlayerController>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public override void Update()
+        {
+            if (Vector3.Distance(_player.transform.position, transform.position) < aggroRadius)
+            {
+                agent.Target = _player.transform.position;
+
+                if (currentChargeTime >= chargeTime)
+                {
+                    animator.SetBool("Charge", false);
+                    rb.velocity = Vector3.zero;
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position, (_player.transform.position - transform.position).normalized, out hit))
+                    {
+                        if (hit.collider.gameObject.tag == "Player")
+                        {
+                            GetComponent<PathfindingAgent>().enabled = false;
+                            chargeDirection = (_player.transform.position - transform.position).normalized;
+                            currentChargeTime = -1;
+                            animator.SetBool("Charge", true);
+                            transform.rotation = Quaternion.LookRotation(chargeDirection);
+                        }
+                    }
+                }
+                else
+                {
+                    currentChargeTime += Time.deltaTime;
+                    if (currentChargeTime < 0)
+                    {
+                        rb.velocity = chargeDirection * chargeSpeed;
+                    }
+                    else
+                    {
+                        GetComponent<PathfindingAgent>().enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
--- Hell's Kitchen/Assets/Scripts/Player/PlayerHealth.cs
+++ Hell's Kitchen/Assets/Scripts/Player/PlayerHealth.cs
@@ -9,6 +9,15 @@ namespace Player
     public class PlayerHealth : MonoBehaviour, IKillable
     {
         [SerializeField] private Animator animator;
+<<<<<<< HEAD
+=======
+        [SerializeField] private float _hitPoints = 100;
+        [SerializeField] private AudioClip takeDamageSound;
+        [SerializeField] private AudioClip lowHealthSound;
+        [SerializeField] private AudioClip deathSound;
+        private float invulnerabilityTime = 1;
+        private float invulnerabilityTimer = 1;
+>>>>>>> d80bcad (Added the pig and added to the dungeon. Fixed animation and added audio)
         private UnityEvent _killed;
         public UnityEvent Killed => _killed ??= new UnityEvent();
 
@@ -17,20 +26,47 @@ namespace Player
             get => GameStateManager.Instance.playerCurrentHitPoints;
             set => GameStateManager.Instance.playerCurrentHitPoints = value;
         }
+<<<<<<< HEAD
         
+=======
+
+        void Update()
+        {
+            if (invulnerabilityTimer < invulnerabilityTime) invulnerabilityTimer += Time.deltaTime;
+        }
+
+>>>>>>> d80bcad (Added the pig and added to the dungeon. Fixed animation and added audio)
         public void TakeDamage(float damage)
         {
-            animator.SetTrigger(PlayerAnimator.TakeHit);
-            HitPoints -= damage;
+            if (invulnerabilityTimer >= invulnerabilityTime)
+            {
+                animator.SetTrigger(PlayerAnimator.TakeHit);
+                HitPoints -= damage;
+                invulnerabilityTimer = 0;
 
-            // If the player's hp is at 0 or lower, they die
-            if (HitPoints <= 0)
-                Die();
+                // If the player's hp is at 0 or lower, they die
+                if (HitPoints <= 0)
+                {
+                    Die();
+                    return;
+                }
+
+                if (HitPoints > 10)
+                {
+                    AudioSource.PlayClipAtPoint(takeDamageSound, transform.position);
+                }
+                else
+                {
+                    AudioSource.PlayClipAtPoint(lowHealthSound, transform.position);
+                }
+            }
         }
 
         private void Die()
         {
+            AudioSource.PlayClipAtPoint(deathSound, transform.position);
             Killed.Invoke();
+            animator.SetTrigger(PlayerAnimator.Dead);
         }
     }
 }
