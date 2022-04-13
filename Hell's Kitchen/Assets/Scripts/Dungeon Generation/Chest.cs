f3dc483b95385a866ef2ab79faff5499acb15296
Daniela Venuta <daniela.venuta27@gmail.com>
2022-04-12 20:24:08 -0400

    Chest Improvements

--- Hell's Kitchen/Assets/Scripts/Dungeon Generation/Chest.cs
+++ Hell's Kitchen/Assets/Scripts/Dungeon Generation/Chest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Common;
 using Common.Enums;
 using Input;
 using UnityEngine;
@@ -10,6 +11,8 @@ namespace Dungeon_Generation
     {
         [SerializeField] private int amountMinInclusive = 20;
         [SerializeField] private int amountMaxExclusive = 40;
+
+        private bool _isLooted = false;
         
         private InputManager _input => InputManager.Instance;
         private Animator _animator;
@@ -30,9 +33,11 @@ namespace Dungeon_Generation
             if (obj.CompareTag(Tags.Player))
             {
                 // TODO ADD UI
-                if (_input.dropItem)
+                if (_input.dropItem && !_isLooted)
                 {
-                    _animator.SetTrigger("OpenChest");
+                    _isLooted = true;
+                    _animator.SetTrigger(ObjectAnimator.OpenChest);
+                    GameStateManager.Instance.cashMoney += GetRandomAmountInRange();
                 }
             }
         }
