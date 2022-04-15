0068a1bf9dd8daa636b87a7ac35ef56cc75684d1
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2022-04-15 12:57:13 -0400

    Weapon Shop UI and Logic (#55)
    * Create Weapon Merchant and Start Shop UI
    
    * Add QuickOutline
    
    * Revert Moving file
    
    * Add outline, fix merge issue
    
    * Outline enabled and disable
    
    * Weapon Shop Functionality
    
    * UI Improvements
    
    * Minor fix
    
    * Deactivate Input on UI open
    
    * Cleanup
    
    * More cleanup
    
    * Bug fix

--- Hell's Kitchen/Assets/Scripts/Common/GameStateManager.cs
+++ Hell's Kitchen/Assets/Scripts/Common/GameStateManager.cs
@@ -19,13 +19,18 @@ namespace Common
         public float cashMoney;
 
         public Dictionary<IRecipe, int> OrderList;
+
+        public List<string> PurchasedWeapons;
+        
         private void Awake()
         {
             if (Instance == null)
+            {
                 Instance = this;
+                Initialize();
+            }
 
             DontDestroyOnLoad(Instance.gameObject);
-            Initialize();
         }
 
         private void Initialize()
@@ -35,6 +40,7 @@ namespace Common
             cashMoney = 0f;
             carriedWeapon = null;
             OrderList = new Dictionary<IRecipe, int>();
+            PurchasedWeapons = new List<string>();
         }
     }
 }
