1a6aea5d9aec7f27aef6b09ef8c355b1e8a40fa4
Daniela Venuta <daniela.venuta27@gmail.com>
2022-04-16 14:59:22 -0400

    Move from dungeon to restaurant

--- Hell's Kitchen/Assets/Scripts/Dungeon Generation/ExitDungeon.cs
+++ Hell's Kitchen/Assets/Scripts/Dungeon Generation/ExitDungeon.cs
@@ -1,6 +1,8 @@
 using Common;
 using Common.Enums;
+using Common.Interfaces;
 using Input;
+using Player;
 using UnityEngine;
 
 namespace Dungeon_Generation
@@ -13,10 +15,24 @@ namespace Dungeon_Generation
         {
             if(other.gameObject.CompareTag(Tags.Player) && _input.interact)
             {
-                if(GameStateManager.Instance.dungeonTimeHasElapsed)
+                if(!GameStateManager.Instance.dungeonTimeHasElapsed)
                     SceneManager.Instance.LoadDungeonScene();
                 else
-                    SceneManager.Instance.LoadDungeonScene();
+                {
+                    var playerController = other.gameObject.GetComponent<PlayerController>();
+                    if (playerController)
+                    {
+                        var heldWeapon = playerController.GetComponentInChildren<IPickup>();
+                        if (heldWeapon != null)
+                        {
+                            GameStateManager.Instance.carriedWeapon = WeaponInstance.None;
+                            heldWeapon.RemoveFromPlayer();
+                        }
+                    }
+                    
+                    SceneManager.Instance.LoadRestaurantScene();
+                }
+                   
             }
         }
     }
