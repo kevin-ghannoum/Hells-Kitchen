62445e1bb5aa992062bdeff23d0ac93a6bc440c7
emilie-martin <emmy.mar@hotmail.com>
2022-04-17 15:45:29 -0400

    create main menu + hook up with camera transition to restaurant scene

---
+++ Hell's Kitchen/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,38 @@
+﻿using Input;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private GameObject canvas;
+        [SerializeField] private Animator animator;
+
+        private void Awake()
+        {
+            title.outlineWidth = .05f;
+            title.outlineColor = Color.black;
+            InputManager.Instance.Deactivate();
+        }
+
+        public void OnPlay()
+        {
+            canvas.SetActive(false); // disable menu UI canvas
+            animator.Play("PlayerCamera"); // camera transition
+            Invoke(nameof(LoadRestaurantScene), .5f); // scene change
+            InputManager.Instance.Activate();
+        }
+
+        private void LoadRestaurantScene()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+
+        public void OnQuit()
+        {
+            Application.Quit();
+        }
+    }
+}
