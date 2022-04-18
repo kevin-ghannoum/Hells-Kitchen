b087367bc04217438f91de8bff17767461a9d261
emilie-martin <emmy.mar@hotmail.com>
2022-04-18 14:06:46 -0400

    add photon networking to coop menu
    - connection to server
    - creating/joining rooms

---
+++ Hell's Kitchen/Assets/Scripts/Server/CoopMenu.cs
@@ -0,0 +1,41 @@
+﻿using Common.Enums;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class CoopMenu : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private TMP_InputField gameRoomInput;
+
+        public void CreateRoom()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.CreateRoom(gameRoomInput.text);
+            }
+            else
+            {
+                Debug.LogError("You must connect to a server first, before creating a room.");
+            }
+        }
+
+        public void JoinRoom()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRoom(gameRoomInput.text);
+            }
+            else
+            {
+                Debug.LogError("You must connect to a server first, before joining a room.");
+            }
+        }
+
+        public override void OnJoinedRoom()
+        {
+            PhotonNetwork.LoadLevel(Scenes.Restaurant);
+        }
+    }
+}
