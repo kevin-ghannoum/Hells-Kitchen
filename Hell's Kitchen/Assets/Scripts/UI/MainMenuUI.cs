e6cbdc9494f1f28f2eae7629fbdabae203d22815
Émilie Martin <44104876+emilie-martin@users.noreply.github.com>
2022-04-18 09:29:40 -0400

    Implement Menus (#71)
    * import main menu title font
    
    * create main menu + hook up with camera transition to restaurant scene
    
    * implement pause menu + integrate inventory in pause menu
    
    * add main menu to build settings
    
    * implement inheritance for menu scripts
    
    * implement game over scene
    
    * add ingredient sprites
    
    * use sprites in ui inventory
    
    * fix forgotten name change
    
    * delete unused prefab
    
    * use layout group for inventory
    
    * add game over scene to game loop
    
    * add OnDestroy for OpenPauseMenu input

---
+++ Hell's Kitchen/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using Input;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainMenuUI : MenuUI
+    {
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private GameObject canvas;
+        [SerializeField] private Animator animator;
+
+        private void Awake()
+        {
+            title.outlineWidth = .05f;
+            title.outlineColor = Color.black;
+            InputManager.Instance.Deactivate();
+        }
+
+        public void OnPlay()
+        {
+            canvas.SetActive(false); // disable menu UI canvas
+            animator.Play("PlayerCamera"); // camera transition
+            Invoke(nameof(LoadRestaurantScene), .5f); // scene change
+            InputManager.Instance.Activate();
+        }
+    }
+}
---
+++ Hell's Kitchen/Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,23 @@
+﻿using Common.Enums;
+using UnityEngine;
+
+namespace UI
+{
+    public class MenuUI : MonoBehaviour
+    {
+        public void LoadRestaurantScene()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(Scenes.Restaurant);
+        }
+
+        public void OnQuit()
+        {
+            Application.Quit();
+        }
+        
+        public void ReturnToMenu()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(Scenes.MainMenu);
+        }
+    }
+}
