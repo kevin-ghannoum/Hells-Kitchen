600341c2099690659333ba873161e38104707077
emilie-martin <emmy.mar@hotmail.com>
2022-04-19 20:26:25 -0400

    add layers to enum

--- Hell's Kitchen/Assets/Scripts/Common/Enums/Layers.cs
+++ Hell's Kitchen/Assets/Scripts/Common/Enums/Layers.cs
@@ -3,5 +3,10 @@
     public class Layers
     {
         public static int Player = 3;
+        public static int UI = 5;
+        public static int Decorations = 6;
+        public static int Enemies = 7;
+        public static int Collectibles = 9;
+        public static int SousChef = 11;
     }
 }
