294150a8ffc8c04c28ba7887c4358b3441b61183
emilie-martin <emmy.mar@hotmail.com>
2022-04-19 21:57:47 -0400

    implement PunRPC for spell calls In SpellManager

--- Hell's Kitchen/Assets/Scripts/SousChef/SpellManager.cs
+++ Hell's Kitchen/Assets/Scripts/SousChef/SpellManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class SpellManager : MonoBehaviour
@@ -9,18 +10,44 @@ public class SpellManager : MonoBehaviour
     [SerializeField] GameObject healSpellPrefab;
     [SerializeField] GameObject knightSkill;
 
-    public void HealerSpell_Photon(GameObject target) {
+    private PhotonView _photonView;
+
+    private void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
+    public void HealerSpell_Photon(GameObject target)
+    {
+        _photonView.RPC(nameof(HealerSpell_Photon_RPC), RpcTarget.All, target);
+    }
+    
+    [PunRPC]
+    public void HealerSpell_Photon_RPC(GameObject target)
+    {
         var obj = Instantiate(photonSpellPrefab, target.transform.position, Quaternion.identity);
         obj.GetComponent<PhotonSpell>().target = target;
-
     }
 
     internal void HealerSpell_Heal(Vector3 position)
     {
+        _photonView.RPC(nameof(HealerSpell_Heal_RPC), RpcTarget.All, position);
+    }
+    
+    [PunRPC]
+    internal void HealerSpell_Heal_RPC(Vector3 position)
+    {
         Instantiate(healSpellPrefab, position, Quaternion.identity);
     }
 
-    public void KnightSkill(){
+    public void KnightSkill()
+    {
+        _photonView.RPC(nameof(KnightSkill_RPC), RpcTarget.All);
+    }
+    
+    [PunRPC]
+    public void KnightSkill_RPC()
+    {
         GameObject slash = Instantiate(knightSkill, transform.position + Vector3.up, Quaternion.LookRotation(transform.forward));
         slash.GetComponent<Slash>().rotation = transform.forward;
     }
