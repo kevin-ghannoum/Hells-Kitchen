07e6abda5d9c019d401fa67c1cb3864f77784ef9
emilie-martin <emmy.mar@hotmail.com>
2022-04-21 11:25:15 -0400

    knight sous-chef code cleanup

--- Hell's Kitchen/Assets/Scripts/SousChef/Knight/States/KnightBaseState.cs
+++ Hell's Kitchen/Assets/Scripts/SousChef/Knight/States/KnightBaseState.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public abstract class KnightBaseState
 {
     public abstract void EnterState(KnightStateManager knight);
--- Hell's Kitchen/Assets/Scripts/SousChef/Knight/States/KnightMoveToTargetState.cs
+++ Hell's Kitchen/Assets/Scripts/SousChef/Knight/States/KnightMoveToTargetState.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KnightMoveToTargetState : KnightBaseState
@@ -11,23 +9,27 @@ public class KnightMoveToTargetState : KnightBaseState
 
     public override void UpdateState(KnightStateManager knight)
     {
-        if(knight.sc.GetDistanceToPlayer() <= knight.sc.searchRange){
+        if(knight.sc.GetDistanceToPlayer() <= knight.sc.searchRange)
+        {
             knight.sc.FindEnemy();
             knight.sc.FindLoot();
         }
         
-
-        if(knight.sc.targetEnemy != null){
-            if(knight.sc.GetDistanceToEnemy() > knight.sc.searchRange){
+        if(knight.sc.targetEnemy != null)
+        {
+            if(knight.sc.GetDistanceToEnemy() > knight.sc.searchRange)
+            {
                 knight.sc.targetEnemy = null;
                 knight.SwitchState(knight.followState);
             }
-            else{
+            else
+            {
                 knight.weapon.GetComponent<Collider>().enabled = false;
                 knight.SwitchState(knight.attackState);
             }
         }
-        else if(knight.sc.targetLoot != null){
+        else if(knight.sc.targetLoot != null)
+        {
             knight.SwitchState(knight.lootState);
         }
     }
