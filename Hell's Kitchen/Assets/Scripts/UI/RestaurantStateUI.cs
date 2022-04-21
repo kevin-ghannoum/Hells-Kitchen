25d435edc56df9376f8d0c654e749a8c4f2f0754
emilie-martin <emmy.mar@hotmail.com>
2022-04-20 20:51:00 -0400

    add open/close UI element to restaurant

---
+++ Hell's Kitchen/Assets/Scripts/UI/RestaurantStateUI.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using Common.Enums;
+using UnityEngine;
+using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace UI
+{
+    public class RestaurantStateUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject openSign;
+        [SerializeField] private GameObject closedSign;
+        private void Start()
+        {
+            if (SceneManager.GetActiveScene().name.Equals(Scenes.Restaurant))
+            {
+                if (GameStateData.hiddenLevel == 0)
+                {
+                    closedSign.SetActive(true);
+                    openSign.SetActive(true);
+                }
+                else
+                {
+                    openSign.SetActive(true);
+                    closedSign.SetActive(false);
+                }
+            }
+        }
+    }
+}
