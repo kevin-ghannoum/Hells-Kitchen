073b2470afb2ab9bac819f3ffa92dc902285efe6
Cristian Milatinov <milatinovcristian@gmail.com>
2022-04-21 02:01:21 -0400

    Fix item drop rpc

--- Hell's Kitchen/Assets/Scripts/PlayerInventory/ItemDrop.cs
+++ Hell's Kitchen/Assets/Scripts/PlayerInventory/ItemDrop.cs
@@ -12,7 +12,7 @@ namespace PlayerInventory
     {
         [SerializeField]
         public ItemInstance item;
-        
+
         [SerializeField]
         public int quantity = 1;
 
@@ -21,24 +21,28 @@ namespace PlayerInventory
 
         private void OnTriggerStay(Collider other)
         {
+            if (!photonView.IsMine)
+                return;
+            
             // items can be picked up by both the chef (player) and sous-chef
             if (other.CompareTag(Tags.Player))
             {
-                var pv = other.GetComponent<PhotonView>();
-                if (pv != null && pv.IsMine)
-                {
-                    photonView.RPC(nameof(PickUp), RpcTarget.All);
-                }
+                photonView.RPC(nameof(PickUpRPC), RpcTarget.All);
             }
-            else if(other.CompareTag(Tags.SousChef)){
+            else if (other.CompareTag(Tags.SousChef))
+            {
                 // check if the sous chef is in loot state and makes sure sous chef is picking up this item
-                if(other.TryGetComponent<HealerStateManager>(out HealerStateManager healerStateManager)){
-                    if(healerStateManager.IsInLootState() && GameObject.ReferenceEquals(other.GetComponent<SousChef>().targetLoot.gameObject, gameObject)){
+                if (other.TryGetComponent(out HealerStateManager healerStateManager))
+                {
+                    if (healerStateManager.IsInLootState() && ReferenceEquals(other.GetComponent<SousChef>().targetLoot.gameObject, gameObject))
+                    {
                         StartCoroutine(PickUpBySousChef(other));
                     }
                 }
-                else if(other.TryGetComponent<KnightStateManager>(out KnightStateManager knightStateManager)){
-                    if(knightStateManager.IsInLootState() && GameObject.ReferenceEquals(other.GetComponent<SousChef>().targetLoot.gameObject, gameObject)){
+                else if (other.TryGetComponent(out KnightStateManager knightStateManager))
+                {
+                    if (knightStateManager.IsInLootState() && ReferenceEquals(other.GetComponent<SousChef>().targetLoot.gameObject, gameObject))
+                    {
                         StartCoroutine(PickUpBySousChef(other));
                     }
                 }
@@ -46,7 +50,7 @@ namespace PlayerInventory
         }
 
         [PunRPC]
-        private void PickUp()
+        private void PickUpRPC()
         {
             AdrenalinePointsUI.SpawnIngredientString(transform.position + 2.0f * Vector3.up, "+ " + quantity + " " + item);
             if (photonView.IsMine)
@@ -56,27 +60,28 @@ namespace PlayerInventory
             }
         }
 
-        IEnumerator PickUpBySousChef(Collider other){
+        IEnumerator PickUpBySousChef(Collider other)
+        {
             // wait untill the sous chef pick up animation ends
             yield return new WaitForSeconds(1.5f);
             var pv = other.GetComponent<PhotonView>();
             if (pv != null && pv.IsMine)
             {
-                photonView.RPC(nameof(PickUp), RpcTarget.All);
+                photonView.RPC(nameof(PickUpRPC), RpcTarget.All);
             }
         }
-        
+
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
             if (stream.IsWriting)
             {
-                stream.SendNext((int)item);
+                stream.SendNext((int) item);
                 stream.SendNext(quantity);
             }
             else if (stream.IsReading)
             {
-                item = (ItemInstance)stream.ReceiveNext();
-                quantity = (int)stream.ReceiveNext();
+                item = (ItemInstance) stream.ReceiveNext();
+                quantity = (int) stream.ReceiveNext();
             }
         }
     }
